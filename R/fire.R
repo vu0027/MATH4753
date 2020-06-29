d6c12089211a315805a7a5b511d0777994fc2944
Anh Vu <46767310+vu0027@users.noreply.github.com>
2020-06-29 16:32:37 -0500

    Add files via upload

---
+++ R/fire.R
@@ -0,0 +1,13 @@
+#' Fire data set.
+#'
+#' A dataset describing the distances of locations from a fire station and their associated fire damage.
+#'
+#'
+#' @format A data frame with  15 rows and 2 variables:
+#' \describe{
+#'   \item{DISTANCE}{distance from fire station, in miles}
+#'   \item{DAMAGE}{fire damage, in thousands of dollars}
+#'   ...
+#' }
+#' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences-Sixth-Edition/Mendenhall-Sincich/p/book/9781498728850}
+"fire"
---
+++ R/myci.R
@@ -0,0 +1,22 @@
+#' 95% confidence interval generating function
+#' Confident interval
+#'
+#' a function that will create a 95% confidence interval for mean x from a single sample x
+#' @param x
+#' vector
+#' @return
+#' confidence interval for the given vector
+#' @export
+#'
+#' @examples
+#' x = rnorm(30, mean = 10, sd = 12)
+#' myci(x)
+#
+myci=function(x){
+  size=length(x)
+  t=qt(1-0.05/2,size-1)
+  ci=c()
+  left=mean(x)-t*sd(x)/sqrt(size)
+  right=mean(x)+t*sd(x)/sqrt(size)
+  sprintf("[%f, %f]", left,right)
+}
